de12da77cf7e56db13e188335ac933e34c3389f6
millenio <milenio@andalsoftware.com>
2025-02-21 09:54:42 +0700

    update path url

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Simple API!");
+});
+
 // Define a route for /simple-api
 app.get("/simple-api", (req, res) => {
   res.send("Hello, Dockerized API at /simple-api!");
