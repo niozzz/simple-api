5c684c4e7372efe25152920d9cb97ee717a01ea8
millenio <milenio@andalsoftware.com>
2025-02-21 13:42:25 +0700

    use env

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Load environment variables from process.env
+const PORT = process.env.PORT;
+
 // Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the Simple API!");
@@ -11,7 +14,7 @@ app.get("/simple-api", (req, res) => {
   res.send("Hello, Dockerized API at /simple-api!");
 });
 
-const PORT = 9999;
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
